584c9b3d2978680faf473c6a19d8fec8aaca6d50
Melany Kunzi <mela74020@gmail.com>
2023-10-17 20:07:24 -0300

    mensaje no habilitado conductor sin vehiculo

--- src/components/Delivery/DeliveryProfile.tsx
+++ src/components/Delivery/DeliveryProfile.tsx
@@ -1,44 +1,59 @@
-"use client"
-import { useEffect, useState } from 'react'
-import { useDeliverys } from '@/context/DeliveryContext'
-import CreateVehicle from '@/components/vehicle/createVehicle'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import { useDeliverys } from '@/context/DeliveryContext';
+import CreateVehicle from '@/components/vehicle/createVehicle';
 import { useVehicles } from '@/context/VehicleContext';
 import VehicleCard from './VehicleCard';
 
 export default function DeliveryProfile({ params }: { params: { id: string } }) {
-    const { deliveryProfile, loadDeliveryProfile } = useDeliverys();
-    const { loadSellerVehicles, sellerCar } = useVehicles();
-    const [cargarAuto, setCargarAuto] = useState(false);
-    const id = params.id;
-    const delivery: any = deliveryProfile;
-    useEffect(() => {
-        loadDeliveryProfile(Number(id));
-        loadSellerVehicles(Number(id));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-    const car:any = sellerCar;
-    console.log(car)
-    return (
-    <div className='bg-gray-600 min-h-screen flex justify-center items-center'>
-    
-    <div className='max-w-md bg-white rounded-lg shadow-lg p-6'>
-      <div className="text-center">
-        <h2 className="text-3xl font-bold text-gray-900">{delivery.name} {delivery.lastName}</h2>
-        <p className="text-gray-700 mb-4">Puntaje Promedio: {delivery.averageScore}</p>
-      </div>
-      <div className="border-t border-gray-200 mt-6 pt-6">
-        <VehicleCard car={car} />
-      </div>
-      <button
-        onClick={() => {
-          setCargarAuto(!cargarAuto);
-        }}
-        className='bg-gray-800 text-white text-xl mt-4 py-2 px-4 rounded-full w-full'>
-        {cargarAuto ? "Cancelar" : "Cargar Nuevo Auto"}
-      </button>
-    </div>
-    {cargarAuto ? <CreateVehicle params={params} /> : null}
-  </div>
+  const { deliveryProfile, loadDeliveryProfile } = useDeliverys();
+  const { loadSellerVehicles, sellerCar } = useVehicles();
+  const [cargarAuto, setCargarAuto] = useState(false);
+  const [showMessage, setShowMessage] = useState(false); // Nuevo estado
+  const id = params.id;
+  const delivery: any = deliveryProfile;
+
+  useEffect(() => {
+    loadDeliveryProfile(Number(id));
+    loadSellerVehicles(Number(id));
+  }, [id]);
+
+  const car: any = sellerCar;
+  console.log(car);
+
+  useEffect(() => {
+    // Verificar si el contenido de car es "vehicle not found" y mostrar el mensaje
+    if (car && car.message === "Vehicle not found") {
+      setShowMessage(true);
+    } else {
+      setShowMessage(false);
+    }
+  }, [car]);
   
-    )
+  return (
+    <div className='bg-gray-600 min-h-screen flex justify-center items-center'>
+      <div className='max-w-md bg-white rounded-lg shadow-lg p-6'>
+        <div className="text-center">
+      {showMessage && (
+        <div className="bg-red-500 text-white p-2 mt-4 text-center rounded">
+          Inhabilitado para repartir: <br /> Debes cargar un vehiculo.
+        </div>
+      )}
+          <h2 className="text-3xl font-bold text-gray-900">{delivery.name} {delivery.lastName}</h2>
+          <p className="text-gray-700 mb-4">Puntaje Promedio: {delivery.averageScore}</p>
+        </div>
+        <div className="border-t border-gray-200 mt-6 pt-6">
+          <VehicleCard car={car} />
+        </div>
+        <button
+          onClick={() => {
+            setCargarAuto(!cargarAuto);
+          }}
+          className='bg-gray-800 text-white text-xl mt-4 py-2 px-4 rounded-full w-full'>
+          {cargarAuto ? "Cancelar" : "Cargar Nuevo Auto"}
+        </button>
+      </div>
+      {cargarAuto ? <CreateVehicle params={params} /> : null}
+    </div>
+  );
 }
--- src/components/Delivery/VehicleCard.tsx
+++ src/components/Delivery/VehicleCard.tsx
@@ -5,7 +5,7 @@ export default function VehicleCard( {car}:{car:Vehicle} ) {
     return (
         <>
               <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Deliverys Vehicle</h5>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Tu Vehiculo</h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">Brand: {car?.brand}</p>
                     <p className="font-normal text-gray-700 dark:text-gray-400">Model: {car?.model}</p>
                     <p className="font-normal text-gray-700 dark:text-gray-400">Color: {car?.color}</p>
