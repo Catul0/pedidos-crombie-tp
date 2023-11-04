942511a1de4099e72ad9530d80831045b63ad9c7
FedericoProgramando <fede_2398@hotmail.com>
2023-11-04 12:34:30 -0300

    Nuevos estilos

--- src/components/home/Imagen.tsx
+++ src/components/home/Imagen.tsx
@@ -4,7 +4,7 @@ function Imagen() {
   const imagenUrl = "https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
   return (
-    <div className="flex flex-col justify-center items-center w-full h-[400px] bg-cover bg-center relative" style={{ backgroundImage: `url(${imagenUrl})` }}>
+    <div className="flex flex-col justify-center items-center w-full h-[400px] bg-cover bg-center" style={{ backgroundImage: `url(${imagenUrl})`, zIndex: 1, marginTop: '73px' }}>
         <div className='flex flex-col justify-center items-center mb-[30px]'>
             <h1 className="text-white text-4xl font-bold animate-bounce">¡Pedí lo que quieras!</h1>
             <p className="text-white text-1xl font-bold">Restaurantes, mercados, kioscos, farmacias y mucho mas.</p>
--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link';
 
 function Navbar() {
   return (
-    <div className="bg-white p-4 pr-20 pl-20 h-18 flex justify-between items-center">
+    <div className='relative'>
+    <div className="bg-white fixed top-0 left-0 w-full h-18 flex justify-between items-center p-4 pr-20 pl-20 z-10" style={{ backgroundColor: 'rgba(255, 255, 255, 0.9)' }}>
       <div className="flex items-center">
         <Image alt="logo" src="/Crombie.png" width={50} height={50} />
       </div>
@@ -20,6 +21,7 @@ function Navbar() {
           </button>
         </Link>
       </div>
+    </div>
     </div>
   );
 }
