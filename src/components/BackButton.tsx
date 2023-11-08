e6164a492f5b297bcf8f673af149964fc1980bf1
catul0 <valesigaudo1@gmail.com>
2023-11-08 14:23:44 -0300

    intento dearreglo de iconos

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { IoMdArrowRoundBack } from "react-icons/Io";
+import { IoMdArrowRoundBack } from "react-icons/io";
 import { useRouter } from "next/navigation";
 export default function BackButton() {
   const router = useRouter();
