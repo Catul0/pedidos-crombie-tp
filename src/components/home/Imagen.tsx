37ef4ddc7d39f02b8d2c2734b541dd43f562ea58
Melany Kunzi <mela74020@gmail.com>
2023-11-10 09:41:34 -0300

    prueba

--- src/components/home/Imagen.tsx
+++ src/components/home/Imagen.tsx
@@ -10,7 +10,7 @@ function Imagen() {
         style={{ backgroundImage: `url(${imagenUrl})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
       ></div>
       <div className='flex flex-col justify-center items-center text-white text-center p-6 space-y-4'>
-        <h1 className="text-4xl font-bold animate-bounce">¡Pedí lo que quieras!</h1>
+        <h1 className="text-4xl font-bold animate-bounce">¡Pedí lo que quieras! 1</h1>
         <p className="text-2xl font-semibold animate-pulse">Restaurantes, mercados, kioscos, farmacias y mucho más.</p>
         <p className="text-2xl font-semibold animate-pulse">Inicia sesión o regístrate para comenzar</p>
       </div>
