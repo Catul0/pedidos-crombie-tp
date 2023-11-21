4182db25f9a449d0044fb699e191c97c0cdbb502
Melany Kunzi <mela74020@gmail.com>
2023-11-21 13:02:50 -0300

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,35 @@ function HomePage() {
       <div className="flex flex-col justify-center items-center mt-[50px] mb-[50px] px-4 sm:px-0">
         <h1 className="text-black text-1x1 font-bold w-full sm:w-[600px] text-center">Sabores que Despiertan los Sentidos, Cuidados que te Confortan, y la Comodidad de Todo lo que Necesitas, Cuando lo Necesitas.🍕❤️</h1>
       </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-10 px-4 md:px-24">
+        <div className="h-4/5 flex flex-col justify-center items-center bg-orange-500 rounded-xl shadow dark:bg-gray-800 dark:border-gray-700 transform transition-all duration-300 hover:scale-105">
+          <div className="h-4/5 p-12 flex justify-center items-center rounded-lg">
+            <img className="h-32 object-cover object-center rounded-t-lg" src='https://images.rappi.com.ar/home-ab-objects/restaurantsicon-1642620539.png?e=webp&d=150x150&q=50' alt="" />
+          </div>
+            <div className="w-full flex justify-center items-center font-semibold h-1/5 bg-orange-100">
+              <h1>Restaurantes</h1>
+            </div>
+        </div>
+        <div className="h-4/5 flex flex-col justify-center items-center bg-green-500 rounded-xl shadow dark:bg-gray-800 dark:border-gray-700 transform transition-all duration-300 hover:scale-105">
+          <div className="h-4/5 p-12 flex justify-center items-center rounded-lg">
+            <img className="h-32 object-cover object-center rounded-t-lg" src='https://images.rappi.com.ar/home-ab-objects/supermarket-1642621254.png?e=webp&d=150x150&q=50' alt="" />
+          </div>
+            <div className="w-full flex justify-center items-center font-semibold h-1/5 bg-green-100">
+              <h1>Supermercados</h1>
+            </div>
+        </div>
+        <div className="h-4/5 flex flex-col justify-center items-center bg-blue-500 rounded-xl shadow dark:bg-gray-800 dark:border-gray-700 transform transition-all duration-300 hover:scale-105">
+          <div className="h-4/5 p-12 flex justify-center items-center rounded-lg">
+            <img className="h-32 object-cover object-center rounded-t-lg" src='https://images.rappi.com.ar/home-ab-objects/pharmacyicon-1642624210.png?e=webp&d=150x150&q=50' alt="" />
+          </div>
+            <div className="w-full flex justify-center items-center font-semibold h-1/5 bg-blue-100">
+              <h1>Farmacias</h1>
+            </div>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center mt-10 mb-10 px-4 sm:px-0">
+        <h1 className="text-black text-5xl font-bold w-full sm:w-[600px] text-center">Únete a Pedidos Crombie</h1>
+      </div>
       <div className="w-full flex flex-col sm:flex-row mb-12 justify-evenly items-center space-y-4 sm:space-y-0">
         <JoinUs
           imageUrl="https://th.bing.com/th/id/OIG.j1P1jlAUGBgBz7Udziy9?pid=ImgGn&w=1024&h=1024&rs=1"
@@ -31,7 +60,6 @@ function HomePage() {
           linkText="Registrate como delivery"
         />
       </div>
-
       <Footer />
     </div>
   );
--- src/components/home/JoinUs.tsx
+++ src/components/home/JoinUs.tsx
@@ -7,18 +7,18 @@ function JoinUs(props: any) {
 
   return (
     <Link href={linkPath}>
-      <div className="w-full md:max-w-md p-6 bg-white border border-gray-300 rounded-md shadow-md dark:bg-gray-800 dark:border-gray-700 transform hover:scale-105 transition-transform">
-        <div className="relative overflow-hidden rounded-lg h-52">
+      <div className="w-full md:max-w-md bg-white border border-gray-300 rounded shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="relative overflow-hidden h-52">
           <img
-            className="w-full h-full object-cover rounded-lg"
+            className="w-full h-full object-cover"
             src={imageUrl}
             alt=""
           />
         </div>
-        <div className="mt-4 text-xl font-semibold text-gray-900 dark:text-white">
+        <div className="m-4 text-xl font-semibold text-gray-900 dark:text-white">
           {paragraphText}
         </div>
-        <div className="mt-2 flex items-center transition-transform transform hover:scale-105">
+        <div className="m-4 flex items-center transition-transform transform hover:scale-105">
           <button className="px-4 py-2 text-sm font-semibold text-green-100 bg-green-500 rounded-lg transition-all hover:bg-green-600 focus:ring-2 focus:ring-offset-2 focus:ring-green-600 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-2 dark:focus:ring-offset-2 dark:focus:ring-blue-800">
             {linkText}
           </button>
