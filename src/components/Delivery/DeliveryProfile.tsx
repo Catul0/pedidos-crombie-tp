b7b0222e62788f727cb7a00d3919041d41a2a43f
Melany Kunzi <mela74020@gmail.com>
2023-11-24 14:22:15 -0300

    volviendo a la version anterior pq no andaba

--- src/components/Delivery/DeliveryProfile.tsx
+++ src/components/Delivery/DeliveryProfile.tsx
@@ -2,51 +2,60 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useDeliverys } from "@/context/DeliveryContext";
+import CreateVehicle from "@/components/vehicle/createVehicle";
 import { useVehicles } from "@/context/VehicleContext";
 import { useOrderContext } from "@/context/OrderContext";
+import VehicleCard from "./VehicleCard";
+import { motion } from "framer-motion";
 import { useUsers } from "@/context/UserContext";
 import { useLocalProfiles } from "@/context/LocalProfileContext";
 import Maps from "../Maps";
 import { useScores } from "@/context/ScoreContext";
-import NavbarDelivery from "./NavbarDelivery";
-import cardInfoDelivery from "./cardInfoDelivery";
-import { motion } from "framer-motion";
 import PuntajeConEstrellas from "../Stars";
-import CreateVehicle from "../vehicle/createVehicle";
-import VehicleCard from "./VehicleCard";
+import BackButton from "../BackButton";
+import { IconUserCircle } from "@tabler/icons-react";
 
 export default function DeliveryProfile({ params }: { params: { id: string } }) {
+	function getAddresses(order: any) {
+		let userAddress = "";
+		let sellerAddress = "";
+
+		const user = users.find((user) => user.id === order.userId);
+
+		if (user) {
+			userAddress = user.address;
+		}
+
+		const seller = localProfiles.find((seller) => seller.id === order.sellerId);
+
+		if (seller) {
+			sellerAddress = seller.address;
+		}
+
+		return { userAddress, sellerAddress };
+	}
+	const { userOrders, handleDeliveryTakingOrder, handleDeliveredOrder } = useOrderContext();
+	const userOrdersFiltered = userOrders.filter(
+		(order: any) =>
+			order.deliveryId === null && order.status != "RECHAZADO" && order.status != "PENDIENTE"
+	);
+	const deliverysOrder = userOrders.filter(
+		(order: any) =>
+			order.deliveryId === Number(params.id) &&
+			order.status != "RECIBIDO" &&
+			order.status != "FINALIZADO"
+	);
+
+	const { loadLocalProfile, localProfiles } = useLocalProfiles();
 	const { deliveryProfile, loadDeliveryProfile } = useDeliverys();
 	const { loadSellerVehicles, sellerCar } = useVehicles();
 	const [cargarAuto, setCargarAuto] = useState(false);
 	const [showMessage, setShowMessage] = useState(false);
-	const { loadUsers, users, loaduserProfile, userProfiles } = useUsers();
+	const { loadUsers, users } = useUsers();
 	const id = Number(params.id);
 	const delivery: any = deliveryProfile;
-	const car: any = sellerCar;
-	const {
-		handleDeliveryTakingOrder,
-		handleDeliveredOrder,
-		loadOrdersInWait,
-		ordersInWait,
-		loadOrderDelivery,
-		deliveryOrder,
-	} = useOrderContext();
-	const { loadLocalProfile, localProfiles, loadSellerProfile, sellerProfile } = useLocalProfiles();
 
-	function getAddresses(order: any) {
-		let sellerAddress = "";
-		loadSellerProfile(order.sellerid);
-		if (sellerProfile && sellerProfile.length > 0) {
-			sellerAddress = sellerProfile[0].address;
-		}
-		let userAddress = "";
-		loaduserProfile(order.userid);
-		if (userProfiles && userProfiles.length > 0) {
-			userAddress = userProfiles[0].address;
-		}
-		return { sellerAddress, userAddress };
-	}
+	const car: any = sellerCar;
 
 	useEffect(() => {
 		// Verificar si el contenido de car es "vehicle not found" y mostrar el mensaje
@@ -56,18 +65,19 @@ export default function DeliveryProfile({ params }: { params: { id: string } }) 
 			setShowMessage(false);
 		}
 	}, [car]);
-
 	const { scores, loadScores } = useScores();
 	const [puntaje, setPuntaje] = useState(0);
 	useEffect(() => {
 		if (scores && scores.length > 0) {
 			const deliveryScores = scores.filter((score) => score.deliveryId === Number(id));
+
 			if (deliveryScores.length > 0) {
-				const totalScore = deliveryScores.reduce(
-					(accumulator, currentScore) => accumulator + currentScore.score,
-					0
-				);
-				setPuntaje(Number((totalScore / deliveryScores.length).toFixed(1)));
+				const totalScore = deliveryScores.reduce((accumulator, currentScore) => {
+					return accumulator + currentScore.score;
+				}, 0);
+				const averageScore = totalScore / deliveryScores.length;
+				const roundedAverageScore = averageScore.toFixed(1);
+				setPuntaje(Number(roundedAverageScore));
 			}
 		}
 	}, [id, scores]);
@@ -79,8 +89,6 @@ export default function DeliveryProfile({ params }: { params: { id: string } }) 
 		}
 	}, [id, delivery, loadScores]);
 	useEffect(() => {
-		loadOrderDelivery(Number(id));
-		loadOrdersInWait();
 		loadLocalProfile();
 		loadUsers();
 		loadSellerVehicles(Number(id));
@@ -88,52 +96,66 @@ export default function DeliveryProfile({ params }: { params: { id: string } }) 
 	const [isInfoOpen, setIsInfoOpen] = useState(false);
 	return (
 		<>
-			<NavbarDelivery setIsInfoOpen={setIsInfoOpen} isInfoOpen={isInfoOpen}/>
+			<div className="bg-white h-16 flex items-center justify-between px-10 border-b shadow-lg">
+				<BackButton />
+				<div className="flex items-center space-x-4">
+					<div className="flex items-center">
+						<div className="flex items-center gap-5">
+							<IconUserCircle
+								width={50}
+								height={50}
+								className="cursor-pointer"
+								onClick={() => setIsInfoOpen(!isInfoOpen)}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
 			<div className="flex justify-center items-center gap-8 p-8 flex-col">
 				{isInfoOpen && (
-							<div className="bg-white rounded-lg shadow-lg p-6">
-							<div className="text-center">
-								{showMessage && (
-									<div className="bg-red-500 text-white p-2 mt-4 text-center rounded">
-										Inhabilitado para repartir: <br /> Debes cargar un vehículo.
-									</div>
-								)}
-								<h2 className="text-3xl font-bold text-gray-900">
-									{delivery.name} {delivery.lastName}
-								</h2>
-								<p className="text-gray-700 mb-4">Puntaje Promedio:</p>
-								<PuntajeConEstrellas puntaje={puntaje} />
-							</div>
-							<div className="border-t border-gray-200 mt-6 pt-6">
-								<VehicleCard car={car} />
-							</div>
-							<button
-								onClick={() => {
-									setCargarAuto(!cargarAuto);
-								}}
-								className={`${
-									cargarAuto
-										? "bg-red-500 hover:bg-red-600 transform scale-105"
-										: "bg-green-500 hover:bg-green-600 transform scale-105"
-								} text-white mt-4 py-2 px-4 rounded-md w-full transition-all duration-500`}>
-								{cargarAuto ? "Cancelar" : "Cargar Nuevo Vehiculo"}
-							</button>
-							{cargarAuto ? (
-								<motion.div
-									initial={{ opacity: 0, y: -10 }}
-									animate={{ opacity: 1, y: 0 }}
-									exit={{ opacity: 0, y: -10 }}
-									transition={{ duration: 0.5, ease: "easeOut" }}>
-									<CreateVehicle params={params} />
-								</motion.div>
-							) : null}
+					<div className="bg-white rounded-lg shadow-lg p-6">
+						<div className="text-center">
+							{showMessage && (
+								<div className="bg-red-500 text-white p-2 mt-4 text-center rounded">
+									Inhabilitado para repartir: <br /> Debes cargar un vehículo.
+								</div>
+							)}
+							<h2 className="text-3xl font-bold text-gray-900">
+								{delivery.name} {delivery.lastName}
+							</h2>
+							<p className="text-gray-700 mb-4">Puntaje Promedio:</p>
+							<PuntajeConEstrellas puntaje={puntaje} />
 						</div>
+						<div className="border-t border-gray-200 mt-6 pt-6">
+							<VehicleCard car={car} />
+						</div>
+						<button
+							onClick={() => {
+								setCargarAuto(!cargarAuto);
+							}}
+							className={`${
+								cargarAuto
+									? "bg-red-500 hover:bg-red-600 transform scale-105"
+									: "bg-green-500 hover:bg-green-600 transform scale-105"
+							} text-white mt-4 py-2 px-4 rounded-md w-full transition-all duration-500`}>
+							{cargarAuto ? "Cancelar" : "Cargar Nuevo Vehiculo"}
+						</button>
+						{cargarAuto ? (
+							<motion.div
+								initial={{ opacity: 0, y: -10 }}
+								animate={{ opacity: 1, y: 0 }}
+								exit={{ opacity: 0, y: -10 }}
+								transition={{ duration: 0.5, ease: "easeOut" }}>
+								<CreateVehicle params={params} />
+							</motion.div>
+						) : null}
+					</div>
 				)}
-				{deliveryOrder.length > 0 ? (
+				{deliverysOrder.length > 0 ? (
 					<div className="bg-white rounded-lg shadow-lg p-6">
 						<h1 className="text-lg font-bold text-gray-900">Tienes un pedido que entregar:</h1>
 						<ul>
-							{deliveryOrder.map((order) => (
+							{deliverysOrder.map((order) => (
 								<li
 									key={order.id}
 									className="mb-4 border border-gray-200 rounded-lg p-4 hover:shadow-md transition-all duration-300">
@@ -190,9 +212,9 @@ export default function DeliveryProfile({ params }: { params: { id: string } }) 
 						<h1 className="text-lg font-bold text-gray-900">
 							No tienes pedidos asignados. Elige uno:
 						</h1>
-						{ordersInWait.length > 0 ? (
+						{userOrdersFiltered.length > 0 ? (
 							<ul>
-								{ordersInWait.map((order) => (
+								{userOrdersFiltered.map((order) => (
 									<li
 										key={order.id}
 										className="mb-4 border border-gray-200 rounded-lg p-4 hover:shadow-md transition-all duration-300">
